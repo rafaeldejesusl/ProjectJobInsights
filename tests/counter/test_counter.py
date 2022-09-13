db3785f7c6f7e17ee3c652662e71c69a6e20f035
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-09-12 21:50:13 -0300

    Requisito 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    result = count_ocurrences("src/jobs.csv", "new")
+    assert(result == 3221)
