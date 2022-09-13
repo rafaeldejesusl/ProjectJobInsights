de15953d5cbb1ae9bcf029283fce00b6cd2281ff
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-09-12 21:59:02 -0300

    Requisito 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    result = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    example = {"title": "Maquinista", "salary": "2000", "type": "trainee"}
+    for item in result:
+        assert(example.keys() == item.keys())
